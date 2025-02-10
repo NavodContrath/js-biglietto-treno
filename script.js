78528d7d0a49060ff16f01cced5c5e4b9ca53b82
NavodContrath <navod.contrath@gmail.com>
2025-02-10 15:10:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/* 
+ Descrizione
+
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+PREPARAZIONE
+- prompt for userKm
+- prompt for userAge
+- const/let for price
+- if/else if/ else for discounts
+- output with innerHTML
+*/
+
+const userKm = Number(prompt("Insert Km that u want to travel[numeric value: '1200']"))
+console.log(userKm);
+
+const userAge = Number(prompt("Insert age of the traveler"))
+console.log(userAge)
